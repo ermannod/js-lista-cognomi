a4a4abe16acf927bb582bc43f9898dec1e2f7d7d
ermannod <erma-d@hotmail.com>
2019-11-18 23:09:39 +0100

    Names

---
+++ script.js
@@ -0,0 +1,22 @@
+// Names
+var names = ["Smith" , "Handerson" , "Jenkins" , "Draper"];
+console.log(names);
+var addedName = prompt("Insert your name");
+console.log(addedName);
+
+var addInArray = names.push(addedName);
+names.sort();
+
+var range = 0;
+var pos = false;
+while ( range < names.length) {
+  console.log(names[range]);
+  console.log("Place in array " + range);
+  range++;
+  pos = true;
+}
+
+if (pos == true ){
+  var turn = names.indexOf(addedName);
+  console.log("Your position in alphabetical order is " + turn);
+}
